dd73800926a6c4884b9182098adb2add7864e470
Viktor <victor99_86@hotmail.com>
2018-02-26 08:42:55 +0100

    Corregido

--- Estructura Datos I/Punteros_ejercicio.cpp
+++ Estructura Datos I/Punteros_ejercicio.cpp
@@ -17,10 +17,10 @@ public:
         dni=100;
         strcpy(nombre, "");
     }
-    Proveedor(int idni)
+    Proveedor(cadena pnombre, int pdni)
     {
-        dni=idni;
-        strcpy(nombre, "");
+        dni=pdni;
+        strcpy(nombre, pnombre);
     }
     int getdni() {return dni;}
 };
@@ -48,25 +48,32 @@ int main()
     //2. Reserva de memoria dinamica heap
     pReal = new float;
     pEntero = new int;
-    pProveedor = new Proveedor();
+    pProveedor = new Proveedor("Pepe",899899898);
 
     //3. Tabla dinamica 100 clientes
     Cliente *tablaClientes = new Cliente[100];
 
     //4. Tabla dinamica de 1000 punteros a clientes incializada a null
-    Cliente *tablaPunterosC[1000];
+    //TABLA ESTAICA: Cliente *tablaPunterosC[1000];
+    //Cliente* es el tipo y el segundo * indica que es dinamico
+    Cliente **tablaPunterosC = new Cliente*[1000];//Tabla dinamica de punteros. Cliente* es el tipo de dato de cada casilla
     for (int i=0; i<1000; i++)
         tablaPunterosC[i]=NULL;
 
     //5. Tabla dinamica de 120 punteros a Prooveedores y crear 120 proveedores
-    Proveedor *tablaPunterosP[120];
+    Proveedor **tablaPunterosP = new Proveedor*[120];
     for (int i=0; i<120; i++)
         tablaPunterosP[i] = new Proveedor();//Actua el constructor sin parametrizar
 
     //Borrar las tablas dinamicas 4 y 5
     delete [] tablaClientes;
-    delete [] tablaPunterosP;
 
+    //delete [] tablaPunterosP; Solamente borra la tabla, pero no los datos a los que apunta
+    for(int i=0; i<120; i++)
+        delete tablaPunterosP[i];//Libera la memoria donde apunta
+    delete [] tablaPunterosP;//Borrar la propia tabla
+
+    //Comprobar como actua el constructor
     int dni = tablaPunterosP[20]->getdni();
     cout << dni;
 
